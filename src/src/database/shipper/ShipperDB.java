8e57def18927f5ea3bf34d46f4d4bd964ed09374
guoxingze <guoxingze@gmail.com>
2013-10-27 00:47:44 -0600

    Add some new return

--- src/src/database/shipper/ShipperDB.java
+++ src/src/database/shipper/ShipperDB.java
@@ -15,9 +15,11 @@ public class ShipperDB implements ShipperDBInterface {
      */
     @Override
     public Object[] getShipmentList() {
-        Object[] returnArr=new String[1];
+        Object[] returnArr=new String[3];
     
         returnArr[0]=1;
+        returnArr[1]=2; //Added by Xingze
+        returnArr[2]=3; //Added by Xingze
         return returnArr;
     }
 
@@ -44,9 +46,9 @@ public class ShipperDB implements ShipperDBInterface {
     @Override
     public Object[] getShipmentProducts(int shipmentId) {
         Object[][] returnArr=new Object[][]{
-                {1,"Nexus 7",30},
-                {2,"Lenovo X230",25},
-                {3,"iPad Mini",15}
+                {1,"Nexus 7",30,"Saskatoon","2013-05-07"},
+                {2,"Lenovo X230",26,"Beijing","2013-06-09"},
+                {3,"iPad Mini",15,"London","2013-06-12"}
             };   
         
         return returnArr;
@@ -113,7 +115,7 @@ public class ShipperDB implements ShipperDBInterface {
     @Override
     public Object[] getStockHandlers() {
         Object[] returnArr=new Object[][]{
-            {1, "Johnny"}
+            {1, "Johnny"},{2,"Amy"},{3,"Bush"}
         };
         
         return returnArr;
