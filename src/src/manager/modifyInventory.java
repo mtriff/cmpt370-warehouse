fdd42191dfe50b51733e29b1e10828cd8e995015
guoxingze <guoxingze@gmail.com>
2013-11-11 18:06:07 -0600

    add class modifyInventory

---
+++ src/src/manager/modifyInventory.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package manager;
+
+import javax.swing.JFrame;
+import warehouseui.InventoryModifyPopup;
+
+/**
+ * modify inventory, change data and location
+ * @author Xingze
+ */
+public class modifyInventory {
+    
+    public static void main(String[] argo){
+        modifyInventory test = new modifyInventory();
+
+        InventoryModifyPopup newInv = new InventoryModifyPopup();
+        newInv.setVisible(true);
+
+        
+    }
+}
